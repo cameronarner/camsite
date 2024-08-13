e0fb763c0e92f9b595cebdbfd53afaf110374dc8
Cameron <carner@its.jnj.com>
2024-08-13 16:47:47 -0400

    updated hover for nav bar and started writing color palette feature

---
+++ Public/homepage.js
@@ -0,0 +1,10 @@
+const frame = document.getElementById("iframe");
+const palette = document.getElementById("paint-sect");
+const book = document.getElementById("book-sect");
+const home = document.getElementById("house-sect");
+
+palette.onclick = function(){
+    frame.style.visibility = "visible";
+}
+
+
